0f26c5df60d44495ee6aa8c0655ca6cc1bb4d878
AbdElRahman Mostafa <68715550+AbdElRahmanMostafa1@users.noreply.github.com>
2022-04-08 15:05:23 +0200

    first commit

--- db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("DB connected successfully"))
-  .catch((e) => console.log(`DB ERROR: ${e}`));
-
-module.exports = mongoose;
--- utils/notFound.js
+++
@@ -1,9 +0,0 @@
-const res = require("express/lib/response");
-
-const notFound = (notFoundObj, status = 404, msg) => {
-  if (notFoundObj) {
-    return res.status(status).send(msg);
-  }
-};
-
-module.exports = notFound;
